5411c311b10f74ec82f8438c9ac1c4723e60f3b1
Asa <chidinmasophia46@gmail.com>
2026-07-21 11:09:21 +0100

    first commit

--- books/models.py
+++ books/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title=models.CharField(max_length=200)
+    author=models.CharField(max_length=150)
+    isbn= models.CharField(max_length=20,unique=True)
+    category= models.CharField(max_length=150)
+    number_of_pages=models.PositiveIntegerField("")
+    quantity=models.PositiveIntegerField("default=1")
+    available=models.PositiveIntegerField("default=1")
+    
+    def __str__(self):
+        return self.title 
+    
--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from books.models import Book
+from members.models import Member
 
-# Create your models here.
+
+class BorrowRecord(models.Model):
+    member = models.ForeignKey(
+        Member,
+        on_delete=models.CASCADE
+    )
+
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE
+    )
+
+    borrow_date = models.DateField(auto_now_add=True)
+    due_date = models.DateField()
+    return_date = models.DateField(
+        null=True,
+        blank=True
+    )
+
+    status = models.CharField(
+        max_length=20,
+        default="Borrowed"
+    )
+
+    def str(self):
+        return f"{self.member} - {self.book}"
