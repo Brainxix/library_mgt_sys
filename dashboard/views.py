4d4dbd1d213ba1dcb2724a1b5cccc7322c6789b6
Brainxix <ezegbuhanson@gmail.com>
2026-08-12 19:47:01 +0100

    made some changes in the  settings

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,7 @@
 
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 
 from books.models import Book
 from members.models import Member
@@ -28,30 +29,29 @@ def dashboard_stats():
 @login_required
 @admin_required
 def admin_dashboard(request):
-    total_books = Book.objects.count()
+    context = dashboard_stats()
+
+    context["today"] = timezone.now()
     
-    total_member = Member.objects.count()
-    
-    available_books = Book.objects.filter(available_copies__gt=0).count()
-    borrowed_books = Book.objects.filter(available_copies__lt=1).count()
-    recent_transactions = (Transactions.objects.select_related("book","member").order_by("-borrow_date")[:5])
-    
-    context = {
-        "today": timezone.now(),
-        "total_books": total_books,
-        "total_members": total_member,
-        "available_books": available_books,
-        "borrowed_books": borrowed_books,
-        "recent_transactions": recent_transactions,
-    }
-    return render(request, "dashboard/admin_dashboard.html", context)
+    context["recent_members"] = Member.objects.order_by("-id")[:5]
+
+    return render(
+        request,
+        "dashboard/admin_dashboard.html",
+        context,
+    )
 
 
 @login_required
 @librarian_required
 def librarian_dashboard(request):
     context = dashboard_stats()
-    return render(request, "dashboard/librarian_dashboard.html", context)
+
+    return render(
+        request,
+        "dashboard/librarian_dashboard.html",
+        context,
+    )
 
 
 @login_required
@@ -60,7 +60,11 @@ def member_dashboard(request):
     context = {
         "borrowed_books": Book.objects.filter(
             available_copies__lt=1
-        ).count()
+        ).count(),
     }
-    return render(request, "dashboard/member_dashboard.html", context)
 
+    return render(
+        request,
+        "dashboard/member_dashboard.html",
+        context,
+    )
